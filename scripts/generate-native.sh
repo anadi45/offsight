fa3bcc222911fb26d6dbf71029dc95b146a11e1a
anadi45 <ranjeetbaraik15@gmail.com>
2025-11-29 08:07:11 +0530

    fix: update generate-native.sh script to use correct app name and clean up template folders

--- scripts/generate-native.sh
+++ scripts/generate-native.sh
@@ -3,17 +3,17 @@ APP_NAME="offsight"
 BUNDLE_ID="com.offsight.app"
 
 echo "Creating clean RN template..."
-npx @react-native-community/cli init ${APP_NAME}Template --package-name "${BUNDLE_ID}" --skip-install
+npx @react-native-community/cli init ${APP_NAME} --package-name "${BUNDLE_ID}" --skip-install
 
 echo "Removing old native folders..."
 rm -rf ios android
 
 echo "Copying fresh native folders..."
-cp -R ${APP_NAME}Template/ios ./ios
-cp -R ${APP_NAME}Template/android ./android
+cp -R ${APP_NAME}/ios ./ios
+cp -R ${APP_NAME}/android ./android
 
 echo "Cleaning template project..."
-rm -rf ${APP_NAME}Template
+rm -rf ${APP_NAME}
 
 echo "Installing pods..."
 cd ios && pod install && cd ..
