731f2803446dcb0b5ad26a3f4a9c9d07c6de980f
anadi45 <ranjeetbaraik15@gmail.com>
2025-11-29 07:23:15 +0530

    fix: build

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    ],
     'react-native-reanimated/plugin',
   ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,7 +7,13 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    alias: {
+      '@': path.resolve(__dirname),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
 
